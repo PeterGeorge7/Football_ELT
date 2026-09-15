fa34484a36cfafb46445d009172fd2df76e214ba
PeterGeorge7 <peto.george.3@gmail.com>
2026-09-16 02:35:45 +0300

    dag updated - handeled working with limit in api, and added the top 5 leagues and 2026 season

--- dags/elt_football_v1.py
+++ dags/elt_football_v1.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime, timezone
+from time import sleep
 from airflow.decorators import dag, task
 from airflow.sensors.base import PokeReturnValue
 import requests
@@ -17,25 +18,57 @@ BUCKET_NAME = "bronze"
 @dag(start_date=datetime(2026, 1, 1), schedule=None, catchup=False)
 def elt_football_v1():
 
+    # sensor to check availability of the api and if it get the limit
     @task.sensor(poke_interval=30, timeout=600, mode="reschedule")
     def check_api_available() -> PokeReturnValue:
         response = requests.get(f"{base_url}competitions", headers=HEADERS)
-        return PokeReturnValue(is_done=response.status_code == 200)
 
-    @task
+        if response.status_code == 200:
+            return PokeReturnValue(is_done=True)
+
+        if response.status_code == 429:
+            reset_seconds = int(response.headers.get("X-RequestCounter-Reset", 10))
+
+            print(
+                f"Rate limit reached. API counter resets in "
+                f"{reset_seconds} seconds."
+            )
+
+            return PokeReturnValue(is_done=False)
+
+        response.raise_for_status()
+
+    @task(pool="api_pool")
     def get_data(topic: str, season: str):
+
         url = f"{base_url}{topic}"
         params = {"season": season, "limit": 500}
+
         start, league, final = topic.split("/")
+
         s3_key = f"{league}/{season}/{league}_{final}_{season}.json"
 
         response = requests.get(url, headers=HEADERS, params=params, timeout=30)
 
         print(f"Request URL: {response.url}")
         print(f"Status code: {response.status_code}")
-        print(f"Content-Type: {response.headers.get('Content-Type')}")
+        print(
+            f"Requests remaining: "
+            f"{response.headers.get('X-Requests-Available-Minute')}"
+        )
+        print(f"Counter reset: " f"{response.headers.get('X-RequestCounter-Reset')}")
+
+        if response.status_code == 429:
+            reset_seconds = int(response.headers.get("X-RequestCounter-Reset", 10))
+
+            print(f"Going to sleep {reset_seconds + 1} and make request.")
+
+            sleep(reset_seconds + 1)
+
+            response = requests.get(url, headers=HEADERS, params=params, timeout=30)
 
         response.raise_for_status()
+
         data = response.json()
 
         S3Hook(aws_conn_id="minio_conn").load_string(
@@ -48,6 +81,9 @@ def elt_football_v1():
         print(
             f"Successfully uploaded {s3_key} to S3-compatible bucket '{BUCKET_NAME}'."
         )
+
+        sleep(7)  # between api requests
+
         return {
             "bucket": "bronze",
             "key": s3_key,
@@ -103,23 +139,46 @@ def elt_football_v1():
 
         print(f"Connection successful! Test result: 1 row inserted")
 
-    pl_topics = [
-        "competitions/PL/standings",
-        "competitions/PL/matches",
-        "competitions/PL/scorers",
-        "competitions/PL/teams",
+    @task.bash
+    def dbt_build():
+        return """
+                cd /usr/local/airflow/dbt/football && 
+                dbt build --profiles-dir /usr/local/airflow/dbt/profiles --project-dir /usr/local/airflow/dbt/football 
+                """
+
+    competitions = ["PL", "SA", "BL1", "FL1", "PD"]
+
+    merged_topics = [
+        [
+            f"competitions/{competition}/standings",
+            f"competitions/{competition}/matches",
+            f"competitions/{competition}/scorers",
+            f"competitions/{competition}/teams",
+        ]
+        for competition in competitions
     ]
+
+    topics = [item for sublist in merged_topics for item in sublist]
+
+    # pl_topics = [
+    #     "competitions/PL/standings",
+    #     "competitions/PL/matches",
+    #     "competitions/PL/scorers",
+    #     "competitions/PL/teams",
+    # ]
 
     check = check_api_available()
 
     metadata = get_data.expand(
-        topic=pl_topics,
-        season=["2023", "2024", "2025"],
+        topic=topics,
+        season=["2023", "2024", "2025", "2026"],
     )
 
     staging_load = staging_loader.expand(metadata=metadata)
 
-    check >> metadata >> staging_load
+    build_dbt = dbt_build()
+
+    check >> metadata >> staging_load >> build_dbt
 
 
 elt_football_v1()
